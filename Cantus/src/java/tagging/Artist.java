1d26f8df911ffd685f114acfe9cd432e8d807ea6
Christopher Raleigh <craleigh318@comcast.net>
2014-11-18 01:22:22 -0800

    Added "Cantus" project.
    Use this new project for future updates.

---
+++ Cantus/src/java/tagging/Artist.java
@@ -0,0 +1,59 @@
+package tagging;
+
+/**
+ * A tag that can store the first name and last name of a song's artist.
+ *
+ * @author Christopher Raleigh
+ * @author Benjamin Ngo
+ * @author Jeremy Wong
+ * @author David-Eric Thorpe
+ */
+public class Artist implements ISongTag {
+
+    private String firstName;
+    private String lastName;
+
+    /**
+     *
+     * @param firstName the forename of the artist
+     * @param lastName the surname of the artist
+     */
+    public Artist(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    /**
+     *
+     * @return the forename of the artist
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     *
+     * @return the surname of the artist
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     *
+     * @return the full name of the artist
+     */
+    public String getFullName() {
+        return (firstName + lastName);
+    }
+
+    @Override
+    public String getTagType() {
+        return "Artist";
+    }
+
+    @Override
+    public String getTagText() {
+        return getFullName();
+    }
+}
