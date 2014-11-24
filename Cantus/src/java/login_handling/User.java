7ead75019756068d33aebf9e5276c2a5315f96e5
Christopher Raleigh <craleigh318@comcast.net>
2014-11-24 14:45:00 -0800

    Removed redundant method in User.

--- Cantus/src/java/login_handling/User.java
+++ Cantus/src/java/login_handling/User.java
@@ -1,6 +1,5 @@
 package login_handling;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -41,12 +40,12 @@ public class User {
      * @throws SQLException
      */
     private static int getUserID(String username) throws SQLException {
-        String query = "SELECT `user_id` FROM `users` WHERE `username` = '";
-        query += username;
-        query += "'";
-        Statement st = server_connections.ConnectionManager.
-                getConnection().prepareStatement(query);
-        ResultSet results = st.executeQuery(query);
+        String value = "user_id";
+        String keyType = "username";
+        String key = "'";
+        key += username;
+        key += "'";
+        ResultSet results = getSQLQuery(value, keyType, key);
         results.next();
         int newID = results.getInt(1);
         return newID;
@@ -74,6 +73,29 @@ public class User {
         return rowCount;
     }
 
+    /**
+     * Returns results from an SQL query.
+     *
+     * @param value the value to get from the row
+     * @param keyType the row to look for the key
+     * @param key the key to search from keyType's column
+     * @return results from the query
+     * @throws SQLException
+     */
+    private static ResultSet getSQLQuery(String value, String keyType,
+            String key) throws SQLException {
+        String query = "SELECT `";
+        query += value;
+        query += "` FROM `users` WHERE `";
+        query += keyType;
+        query += "` = ";
+        query += key;
+        Statement st = server_connections.ConnectionManager.
+                getConnection().createStatement();
+        ResultSet results = st.executeQuery(query);
+        return results;
+    }
+
     private final int id;
 
     /**
@@ -92,16 +114,11 @@ public class User {
      * @return results from the query
      * @throws SQLException
      */
-    private ResultSet getSQLQuery(String value) throws
+    private ResultSet getSQLQueryFromUserID(String value) throws
             SQLException {
-        String query = "SELECT `";
-        query += value;
-        query += "` FROM `users` WHERE `user_id` = ";
-        query += Integer.toString(id);
-        query += "";
-        PreparedStatement st = server_connections.ConnectionManager.
-                getConnection().prepareStatement(query);
-        ResultSet results = st.executeQuery(query);
+        String keyType = "user_id";
+        String key = Integer.toString(id);
+        ResultSet results = getSQLQuery(value, keyType, key);
         return results;
     }
 
@@ -109,8 +126,9 @@ public class User {
      *
      * @return this user's name
      */
-    String getUsername() throws SQLException {
-        ResultSet results = getSQLQuery("username");
+    String getUsername()
+            throws SQLException {
+        ResultSet results = getSQLQueryFromUserID("username");
         results.next();
         String username = results.getString(1);
         return username;
@@ -121,9 +139,8 @@ public class User {
      * @return this user's password
      * @throws SQLException
      */
-    private String getPassword()
-            throws SQLException {
-        ResultSet results = getSQLQuery("password");
+    private String getPassword() throws SQLException {
+        ResultSet results = getSQLQueryFromUserID("password");
         results.next();
         String password = results.getString(1);
         return password;
